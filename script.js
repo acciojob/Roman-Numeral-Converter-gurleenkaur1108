c33cb9bb0f862160ff2960b082230577e688edcd
gurleenkaur1108 <76990139+gurleenkaur1108@users.noreply.github.com>
2024-01-29 14:32:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  const obj = {
+    0: ['M', 1000],
+    1: ['D', 500],
+    2: ['C', 100],
+    3: ['L', 50],
+    4: ['X', 10],
+    5: ['V', 5],
+    6: ['I', 1]
+  };
 
-  //your code here
+  let result = '';
 
+  for (let i = 0; i < 7; i += 2) {
+    const [symbol1, value1] = obj[i];
+    const [symbol2, value2] = obj[i - 1];
+    const [symbol3, value3] = obj[i - 2];
+
+    const quotient = Math.floor(num / value1);
+    num %= value1;
+
+    if (quotient === 9) {
+      result += symbol3 + symbol1; // Subtractive notation (e.g., CM for 900)
+    } else if (quotient >= 5) {
+      result += symbol2;
+      result += symbol1.repeat(quotient - 5);
+    } else if (quotient === 4) {
+      result += symbol3 + symbol2; // Subtractive notation (e.g., IV for 4)
+    } else {
+      result += symbol1.repeat(quotient);
+    }
+  }
+
+  return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+// Testing the function with input 36
+console.log(convertToRoman(36));
 
+module.exports = convertToRoman;
 
-
-
-// do not edit below this line
-module.exports = convertToRoman
